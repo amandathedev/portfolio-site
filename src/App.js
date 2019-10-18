740fbb58ba105386ab741e0cd7dc7e21eef5f6cb
Amanda Treutler <amandathedev@gmail.com>
2019-10-17 21:41:30 -0500

    background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio</h1>
+      <Content />
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
+import "../styles/content.css";
 
 export default class Content extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div class="bg"></div>
+        <div class="bg bg2"></div>
+        <div class="bg bg3"></div>
+      </div>
+    );
   }
 }
