c304bd5587cacde9bba6254f779e2d4c7f7978ac
Amanda Treutler <amandathedev@gmail.com>
2019-10-19 14:34:25 -0500

    spacing

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,19 @@ import React from "react";
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      {/* <h1>About</h1> */}
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img
+            src="/docs/4.3/assets/brand/bootstrap-solid.svg"
+            width="30"
+            height="30"
+            class="d-inline-block align-top"
+            alt=""
+          />
+          TEST NAVBAR
+        </a>
+      </nav>
     </div>
   );
 };
--- src/components/Content.js
+++ src/components/Content.js
@@ -6,14 +6,15 @@ export default class Content extends Component {
   render() {
     return (
       <div>
-        <h1>
-          {/* <i class="fas fa-chevron-right"></i> */}
-          AMANDA TREUTLER
-        </h1>
-        <h4>Software Engineer</h4>
-        <button className="btn draw">Learn More</button>
-        {/* Background animation */}
         <div className="background-animation">
+          <h1>
+            {/* <i class="fas fa-chevron-right"></i> */}
+            AMANDA TREUTLER
+          </h1>
+          <h4>Software Engineer</h4>
+          <button className="button draw">Learn More</button>
+          {/* Background animation */}
+
           <div className="bg"></div>
           <div className="bg bg2"></div>
           <div className="bg bg3"></div>
