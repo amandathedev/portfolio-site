5b5af77dfc8f1388e1ab3b03df3b299fb237e23e
Amanda Treutler <amandathedev@gmail.com>
2019-10-18 19:51:31 -0500

    landing styling

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,6 +5,13 @@ export default class Content extends Component {
   render() {
     return (
       <div>
+        <h1>
+          {/* <i class="fas fa-chevron-right"></i> */}
+          AMANDA TREUTLER
+        </h1>
+        <h4>Software Engineer</h4>
+        <button class="btn draw">Learn More</button>
+        {/* Background animation */}
         <div class="bg"></div>
         <div class="bg bg2"></div>
         <div class="bg bg3"></div>
