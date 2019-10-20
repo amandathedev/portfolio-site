ae86d39424f0c34d751cc1e1c88b423d18a47978
Amanda Treutler <amandathedev@gmail.com>
2019-10-20 11:14:59 -0500

    completely changes layout

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,19 +5,20 @@ import "../styles/content.css";
 export default class Content extends Component {
   render() {
     return (
-      <div>
-        <div className="background-animation">
+      <div className="main-box">
+        <div className="title">
           <h1>
-            {/* <i class="fas fa-chevron-right"></i> */}
-            AMANDA TREUTLER
+            Amanda
+            <br />
+            Treutler
           </h1>
           <h4>Software Engineer</h4>
-          <button className="button draw">Learn More</button>
+          {/* <button className="button draw">Learn More</button> */}
           {/* Background animation */}
 
-          <div className="bg"></div>
+          {/* <div className="bg"></div>
           <div className="bg bg2"></div>
-          <div className="bg bg3"></div>
+          <div className="bg bg3"></div> */}
         </div>
       </div>
     );
