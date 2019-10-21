7c45c5a4ca38fca45100747d54766c47569c1bc6
Amanda Treutler <amandathedev@gmail.com>
2019-10-21 11:06:21 -0500

    photo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import About from "./components/About";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navbar />
+      <About />
     </div>
   );
 }
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <div>
+      <img className="main-photo" src="https://i.imgur.com/PJ3SD5X.jpg"></img>
+    </div>
+  );
+};
+
+export default About;
