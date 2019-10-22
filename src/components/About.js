f52055dbf8b4b34d3a66d03f8c169b2a5feb8d36
Amanda Treutler <amandathedev@gmail.com>
2019-10-22 11:06:45 -0500

    header styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import "../styles/about.css";
 
 const About = () => {
   return (
-    <div>
+    <div className="about-div">
+      <h2>About</h2>
+      <div className="underline"></div>
       <img className="main-photo" src="https://i.imgur.com/PJ3SD5X.jpg"></img>
     </div>
   );
