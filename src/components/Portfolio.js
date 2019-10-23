351df98c2068817cfe0eea835d3f2ffdd4a250db
Amanda Treutler <amandathedev@gmail.com>
2019-10-23 09:46:19 -0500

    big screen

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,16 +7,72 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className="underline"></div>
 
-      {/* Grid */}
-      <div class="portfolio-grid">
-        <div class="item1">
-          {" "}
-          <div class="computer-1">
-            <div class="computer-screen">
-              <div class="computer-screen-1"></div>
+      <div className="big-screen">
+        <div
+          id="carouselExampleFade"
+          class="carousel slide carousel-fade"
+          data-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                src="https://i.imgur.com/ifUChnn.png"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://i.imgur.com/JAjrDdU.png"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://i.imgur.com/Jrg8YVm.png"
+                class="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div class="carousel-item">
+              <img
+                src="https://i.imgur.com/F75dN6c.png"
+                class="d-block w-100"
+                alt="..."
+              />
             </div>
           </div>
+          <a
+            class="carousel-control-prev"
+            href="#carouselExampleFade"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              class="carousel-control-prev-icon carousel-icon"
+              aria-hidden="true"
+            ></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleFade"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              class="carousel-control-next-icon carousel-icon"
+              aria-hidden="true"
+            ></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
+      </div>
+
+      {/* Grid */}
+      {/* <div class="portfolio-grid">
+        <div class="item1"></div>
         <div class="item2">
           <h5>SpeakTo</h5>
           <p>Here is a description of SpeakTo.</p>
@@ -34,7 +90,7 @@ const Portfolio = () => {
         <div class="item4"></div>
         <div class="item5">photo3</div>
         <div class="item6">text3</div>
-      </div>
+      </div> */}
     </div>
   );
 };
