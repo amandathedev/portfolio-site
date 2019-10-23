f50d60455ad25bde2cb3761506dc09fca51e7fdd
Amanda Treutler <amandathedev@gmail.com>
2019-10-23 08:30:57 -0500

    grid

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,16 +6,35 @@ const Portfolio = () => {
     <div className="portfolio-div">
       <h2>Portfolio</h2>
       <div className="underline"></div>
-      <div class="computer-1">
-        <div class="computer-screen">
-          <div class="computer-screen-1"></div>
+
+      {/* Grid */}
+      <div class="portfolio-grid">
+        <div class="item1">
+          {" "}
+          <div class="computer-1">
+            <div class="computer-screen">
+              <div class="computer-screen-1"></div>
+            </div>
+          </div>
         </div>
+        <div class="item2">
+          <h5>SpeakTo</h5>
+          <p>Here is a description of SpeakTo.</p>
+          <p>
+            <strong>Built with:</strong> React, Ruby on Rails (API), Bootstrap
+          </p>
+        </div>
+        <div class="item3">
+          <h5>GrubCrud</h5>
+          <p>Here is a description of GrubCrud.</p>
+          <p>
+            <strong>Built with:</strong> Ruby on Rails, Bootstrap
+          </p>
+        </div>
+        <div class="item4"></div>
+        <div class="item5">photo3</div>
+        <div class="item6">text3</div>
       </div>
-      {/* <div class="computer-2">
-        <div class="computer-screen">
-          <div class="computer-screen-1"></div>
-        </div>
-      </div> */}
     </div>
   );
 };
